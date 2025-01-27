c7339718a903e395e59165fcba5b70ec55ae9c61
PremKumar <143885129+PremKumardb19@users.noreply.github.com>
2025-01-27 21:49:20 +0530

    Updated render url

--- RecipeApp/src/components/Register.tsx
+++ RecipeApp/src/components/Register.tsx
@@ -12,7 +12,7 @@ const Register = () => {
     e.preventDefault();
     
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/register", {
+      const response = await axios.post("https://recipes-uhra.onrender.com/api/auth/register", {
         name,
         email,
         password,

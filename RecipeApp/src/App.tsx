5da853f758434d472ab462ff61c44d759a8de117
PremKumar <143885129+PremKumardb19@users.noreply.github.com>
2025-01-27 21:59:15 +0530

    Update App.tsx

--- RecipeApp/src/App.tsx
+++ RecipeApp/src/App.tsx
@@ -1,44 +1,42 @@
-import "./App.css";
-import { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Explore from "./pages/Explore";
-import Home from "./pages/Home";
+import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import Home from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Info from "./pages/Info";
-import Login from "./components/Login"
-import Register from "./components/Register"
-import ProtectedRoute from "./components/ProtectedRoute";
+import Info from './pages/Info';
+import Login from './components/Login';
+import Register from './components/Register';
+import ProtectedRoute from './components/ProtectedRoute';
 
+const App: React.FC = () => {
+  const [isLogged, setIsLogged] = useState<boolean>(false);
 
-function App() {
-  
-  const [isLogged,setIsLogged]=useState(false)
   return (
     <>
       <Routes>
-       
-        <Route path="/login" element={<Login setIsLogged={setIsLogged}/>} />
+        <Route path="/login" element={<Login setIsLogged={setIsLogged} />} />
         <Route path="/register" element={<Register />} />
         <Route
           path="/explore"
           element={
-            <ProtectedRoute >
-              <Explore isLogged={isLogged} setIsLogged={setIsLogged}/>
+            <ProtectedRoute>
+              <Explore isLogged={isLogged} setIsLogged={setIsLogged} />
             </ProtectedRoute>
           }
         />
         <Route
           path="/info/:id"
           element={
-            <ProtectedRoute >
-              <Info isLogged={isLogged} setIsLogged={setIsLogged}/>
+            <ProtectedRoute>
+              <Info isLogged={isLogged} setIsLogged={setIsLogged} />
             </ProtectedRoute>
           }
         />
-        <Route index element={<Home isLogged={isLogged} setIsLogged={setIsLogged}/>} />
+        <Route index element={<Home isLogged={isLogged} setIsLogged={setIsLogged} />} />
       </Routes>
     </>
   );
-}
+};
 
 export default App;

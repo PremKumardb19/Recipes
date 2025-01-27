5ea46be65647d8dcc31ce52d0be548ac2b63e4bf
PremKumar <143885129+PremKumardb19@users.noreply.github.com>
2025-01-27 21:47:53 +0530

    Updated render url

--- RecipeApp/src/components/Login.tsx
+++ RecipeApp/src/components/Login.tsx
@@ -11,7 +11,7 @@ const Login = ({setIsLogged}) => {
     e.preventDefault();
     
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/login", {
+      const response = await axios.post("https://recipes-uhra.onrender.com/api/auth/login", {
         email,
         password,
       });

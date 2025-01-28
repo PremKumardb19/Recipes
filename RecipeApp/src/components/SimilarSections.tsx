d1d5876f807802fddfc281a14d50512b513c765d
PremKumar <143885129+PremKumardb19@users.noreply.github.com>
2025-01-28 06:46:17 +0530

    Update SimilarSections.tsx

--- RecipeApp/src/components/SimilarSections.tsx
+++ RecipeApp/src/components/SimilarSections.tsx
@@ -1,28 +1,33 @@
 import { useNavigate } from "react-router-dom";
-import Cards from "./Cards"
-import Spinner from "react-bootstrap/Spinner"
-const SimilarSection=({similar})=>{
-  if(similar===null){
-    return (<></>
-      )
-      
-  }
-    const navigate=useNavigate()
-    const redirectToInfoPage = (id) => {
-        navigate(`/info/${id}`);
-      };
-      console.log("SIMIlar section rendreec",similar)
-   return (
-      <div className="container-fluid mt-4">
-          <div className="cards-container">
-            <h1 className="fs-2 fw-bold card-title text-center"
-            style={{
-              color:"orangered"
-            }} >Similar Recipes</h1>
-              {similar && <Cards results={similar}  redirectToInfoPage={redirectToInfoPage}/>}
-          </div>
-      </div>
-   )
+import Cards from "./Cards";
+import Spinner from "react-bootstrap/Spinner";
+
+interface SimilarSectionProps {
+  similar: { id: number; title: string }[] | null; // Adjust this type based on the shape of `similar`
 }
 
-export default SimilarSection
+const SimilarSection = ({ similar }: SimilarSectionProps) => {
+  if (similar === null) {
+    return <></>;
+  }
+
+  const navigate = useNavigate();
+  const redirectToInfoPage = (id: number) => {
+    navigate(`/info/${id}`);
+  };
+
+  console.log("SIMIlar section rendered", similar);
+
+  return (
+    <div className="container-fluid mt-4">
+      <div className="cards-container">
+        <h1 className="fs-2 fw-bold card-title text-center" style={{ color: "orangered" }}>
+          Similar Recipes
+        </h1>
+        {similar && <Cards results={similar} redirectToInfoPage={redirectToInfoPage} />}
+      </div>
+    </div>
+  );
+};
+
+export default SimilarSection;

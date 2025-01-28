f9e846afaa04abfb64dab2ab918de6887029f773
PremKumar <143885129+PremKumardb19@users.noreply.github.com>
2025-01-28 05:40:19 +0530

    Update main.tsx

--- RecipeApp/src/main.tsx
+++ RecipeApp/src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
-import App from './App.tsx'
+import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  
-  <BrowserRouter>
-    <App />
+  <StrictMode>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
-)
+  </StrictMode>
+);

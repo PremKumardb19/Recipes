c73ac317d875aca4ad84e4a61c32eed8fd7b668f
PremKumar <143885129+PremKumardb19@users.noreply.github.com>
2025-01-28 08:27:07 +0530

    Update Info.tsx

--- RecipeApp/src/pages/Info.tsx
+++ RecipeApp/src/pages/Info.tsx
@@ -1,3 +1,11 @@
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { fetchData } from "../utils/GetterFns";
+import { useParams } from "react-router-dom";
+import InfoHero from "../components/InfoHero";
+import Switcher from "../components/Switcher";
+import SimilarSection from "../components/SimilarSections";
+
 interface Ingredient {
   name: string;
   amount: number;
@@ -11,7 +19,7 @@ interface RecipeDetails {
   extendedIngredients: Ingredient[];
 }
 
-const Info = ({ isLogged, setIsLogged }: InfoProps) => {
+const Info = ({ isLogged, setIsLogged }: { isLogged: boolean; setIsLogged: React.Dispatch<React.SetStateAction<boolean>> }) => {
   const [details, setDetails] = useState<RecipeDetails | null>(null);
   const [similar, setSimilar] = useState<any | null>(null);
   const { id } = useParams();
@@ -29,7 +37,7 @@ const Info = ({ isLogged, setIsLogged }: InfoProps) => {
       const transformedIngredients = data.extendedIngredients.map((ingredient: any) => ({
         name: ingredient.name,
         amount: ingredient.amount,
-        aisle: ingredient.aisle || '', // Add default empty values for missing properties
+        aisle: ingredient.aisle || '',
         consistency: ingredient.consistency || '',
         unit: ingredient.unit || '',
       }));
